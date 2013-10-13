3853f80e4279fec6d0666249f4b1b9f13e916346
Rob Clark <robclark@freedesktop.org>
2013-10-13 09:49:09 -0400

    squash! libdrm: atomic mode set

--- include/drm/drm_mode.h
+++ include/drm/drm_mode.h
@@ -514,9 +514,15 @@ struct drm_mode_destroy_dumb {
 	__u32 handle;
 };
 
-#define DRM_MODE_ATOMIC_TEST_ONLY (1<<0)
-#define DRM_MODE_ATOMIC_EVENT (1<<1)
-#define DRM_MODE_ATOMIC_NONBLOCK (1<<2)
+/* page-flip flags are valid, plus: */
+#define DRM_MODE_ATOMIC_TEST_ONLY 0x0100
+#define DRM_MODE_ATOMIC_NONBLOCK  0x0200
+
+#define DRM_MODE_ATOMIC_FLAGS (\
+		DRM_MODE_PAGE_FLIP_EVENT |\
+		DRM_MODE_PAGE_FLIP_ASYNC |\
+		DRM_MODE_ATOMIC_TEST_ONLY |\
+		DRM_MODE_ATOMIC_NONBLOCK)
 
 /* FIXME come up with some sane error reporting mechanism? */
 struct drm_mode_atomic {
